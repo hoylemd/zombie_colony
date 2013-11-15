862ebf0717260669802eea8553e55a9410de8973
Michael D. Hoyle <hoylemd@gmail.com>
2013-11-14 20:26:46 -0500

    #2: Added the gui bar sections

--- Assets/Scripts/main_bar.cs
+++ Assets/Scripts/main_bar.cs
@@ -2,27 +2,68 @@
 using System.Collections;
 
 public class main_bar : MonoBehaviour {
-	private int height = 80;
-	private int horizontal_offset = 10;
-	private int vertical_offset = 10;
+	// general UI sizes and offsets
+	private Rect rectangle;
 	
+	// measurements for the sections
+	private int character_width;
+	private int commands_width;
+	private int minimap_width;
+	
+	// initialize the sizes and offsets
 	void Start () {
-		this.height = 80;
-		this.horizontal_offset = 10;
-		this.vertical_offset = 10;
-		Debug.Log ("wat");
+		// initial offsets
+		int horizontal_offset = 10;
+		int vertical_offset = 10;
+		int height = 180;
+		
+		// initialize character information measurements
+		this.character_width = 200;
+		this.commands_width = 948;
+		this.minimap_width = 200;
+		
+		// set up the rectangle
+		this.rectangle = new Rect(
+			horizontal_offset,
+			Screen.height - (vertical_offset + height),
+			Screen.width - (2 * horizontal_offset),
+			height
+		);
 	}
 	
 	void OnGUI () {
-		// make the background box
+
+		// make the character section
 		GUI.Box (
 			new Rect(
-				this.horizontal_offset,
-				Screen.height - (this.vertical_offset + this.height),
-				Screen.width - (2 * this.horizontal_offset), this.height
+				this.rectangle.x,
+				this.rectangle.y,
+				this.character_width,
+				this.rectangle.height
 			),
-			"Main Bar"
-		);
+			"Character Information"
+		);	
 		
+		// make the commands section
+		GUI.Box (
+			new Rect(
+				this.rectangle.x + this.character_width,
+				this.rectangle.y,
+				this.commands_width,
+				this.rectangle.height
+			),
+			"Commands"
+		);	
+		
+		// make the minimap section
+		GUI.Box (
+			new Rect(
+				this.rectangle.x + this.character_width + this.commands_width,
+				this.rectangle.y,
+				this.minimap_width,
+				this.rectangle.height
+			),
+			"Minimap"
+		);	
 	}
 }
