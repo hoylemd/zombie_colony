c7d19458a5e07acfcd99316cc8b758665f83c606
Michael D. Hoyle <hoylemd@gmail.com>
2013-11-17 17:00:28 -0500

    #8: added zombie
    Made zombie object
    moved player control out into Player class

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,15 +4,14 @@ using System.Collections;
 public class Character : MonoBehaviour {
 	public int hp;
 	public int max_hp;
-	public int speed;
+	public int speed; 
 	
-	private Vector3 targetPosition;
-	private float journeyLength;
+	public Vector3 targetPosition;
+	public float journeyLength;
 	
-    private float startTime;
+    public float startTime;
 	// Use this for initialization
 	void Start () {
-		this.name = "character";
 		this.max_hp = 10;
 		this.hp = this.max_hp;
 		this.speed = 1;
@@ -20,23 +19,7 @@ public class Character : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-		// Mouse moves and attacks
-		if(Input.GetKeyDown(KeyCode.Mouse0)){
-			Plane playerPlane = new Plane(Vector3.up, Vector3.zero);
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			float hitdist = 0.0f;
-        	startTime = Time.time;
-			
-			if (playerPlane.Raycast(ray, out hitdist)) {
-				targetPosition = ray.GetPoint(hitdist);
-				targetPosition = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
-				transform.rotation = Quaternion.LookRotation(targetPosition - transform.position);
-        		journeyLength = Vector3.Distance(transform.position, targetPosition);
-			}
-		}
-		
+	public void Update () {
 		// Move if we are not paused
 		if (Time.timeScale != 0) {
 			if (targetPosition != transform.position) {
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : Character {
+	
+	// Update is called once per frame
+	new void Update () {
+	// Mouse moves and attacks
+		if(Input.GetKeyDown(KeyCode.Mouse0)){
+			Plane playerPlane = new Plane(Vector3.up, Vector3.zero);
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			float hitdist = 0.0f;
+        	startTime = Time.time;
+			
+			if (playerPlane.Raycast(ray, out hitdist)) {
+				targetPosition = ray.GetPoint(hitdist);
+				targetPosition = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
+				transform.rotation = Quaternion.LookRotation(targetPosition - transform.position);
+        		journeyLength = Vector3.Distance(transform.position, targetPosition);
+			}
+		}
+		
+		base.Update();
+		
+	}
+}
